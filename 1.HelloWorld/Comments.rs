d51ae9e6f492241a6c069c721c21b8675f8c9e59
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2024-07-30 19:04:09 +0530

    Introduction to Rust
    Hello World, Comments , formated Print in rust

---
+++ 1.HelloWorld/Comments.rs
@@ -0,0 +1,12 @@
+fn main(){
+    println!("Single Line Commnets  Start with //");
+    // Single Line comments in Rust
+    println!("Block  Commnets  Start with /*");
+    /*Block Comments in rust*/
+    /*
+    // Apart from this we also have Doc Comments
+    /// Generate Library docs for Following items
+    //! Generate Library docs for enclosing items
+    */
+
+}
